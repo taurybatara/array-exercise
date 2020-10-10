b85abfc11f957155a648ad88211214216343ca58
Taury Ko <tauryko@hotmail.com>
2020-10-10 16:05:39 +0200

    Questions 1 - 10

--- e.js
+++ e.js
@@ -22,14 +22,128 @@ main();
 function main() {
     console.log('Hello');
     one([...people]);
+    oneMitch([...people]);
+    two([...people]);
+    three([...people]);
+    four([...people]);
+    five([...people]);
+    six([...people]);
+    sixMitch([...people]);
+    seven([...people]);
+    eight([...people]);
+    nine([...people]);
+    ten([...people]);
 }
 
 function one(people) {
     console.log('Vraag 1');
-
     for (let i = 0; i < people.length; i++) {
         const person = people[i];
-        console.log((i+1) + ". " + person);
+        console.log((i + 1) + ". " + person);
     }
 }
 
+function oneMitch(poeple) {
+    printVraag(1);
+    printPeople(people);
+}
+
+function cSpecial(string) {
+    console.log(string + '12.5%');
+}
+
+function printPeople(people) {
+    for (let index = 0; index < people.length; index++) {
+        cSpecial(people[index]);
+    }
+}
+
+function printVraag(number) {
+    console.log(`Vraag ${number}.`)
+}
+
+function two(people) {
+    console.log('Vraag 2')
+    people.splice(2, 1)
+    for (let i = 0; i < people.length; i++) {
+        const person = people[i];
+        console.log((i + 1) + ". " + person);
+    }
+}
+
+function three(people) {
+    console.log('Vraag 3')
+    people.splice(0, 1)
+    for (let i = 0; i < people.length; i++) {
+        const person = people[i];
+        console.log((i + 1) + ". " + person);
+    }
+}
+
+function four(people) {
+    console.log('Vraag 4')
+    people.unshift("Zacharia")
+    for (let i = 0; i < people.length; i++) {
+        const person = people[i];
+        console.log((i + 1) + ". " + person);
+    }
+}
+
+function five(people) {
+    console.log('Vraag 5')
+    people.push('Precinio')
+    for (let i = 0; i < people.length; i++) {
+        const person = people[i];
+        console.log((i + 1) + ". " + person);
+    }
+}
+
+function six(people) {
+    console.log('Vraag 6');
+    for (let i = 0; i < 3; i++) {
+        const person = people[i];
+        console.log((i + 1) + ". " + person);
+    }
+}
+
+function sixMitch(people) {
+    console.log('Vraag 6 Mitch');
+    for (let i = 0; i < people.length; i++) {
+        const person = people[i];
+        
+        console.log(`${i + 1} ${person}`);
+
+        if (person === "Brandon") {
+            i = people.length;
+        }
+    }
+}
+
+function seven(people) {
+    console.log('Vraag 7');
+    people.slice(0, 1)
+    for (let i = 0; i < people.length; i++) {
+        const person = people[i];
+        console.log((i + 1) + ". " + person);
+    }
+}
+
+function eight(people) {
+    console.log('Vraag 8');
+    console.log(`The index of "Taury" is: ${people.indexOf('Taury')}`);
+}
+
+function nine(people) {
+    console.log('Vraag 9');
+    console.log(`The index of "Foo" is: ${people.indexOf('Foo')}`);
+}
+
+function ten(people) {
+    console.log('Vraag 10');
+    people.splice(1, 1);
+    people.push('Zacharia', 'Precinio');
+    for (let i = 0; i < people.length; i++) {
+        const person = people[i];
+        console.log((i + 1) + ". " + person);
+    }
+}
